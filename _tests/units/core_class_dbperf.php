524c933f81484505b928d54acbd42a27e9916ff8
Lennart Schmidt <pernicius@web.de>
2011-06-30 06:50:09 +0200

    class DBPerf should be done now

--- _tests/units/core_class_dbperf.php
+++ _tests/units/core_class_dbperf.php
@@ -31,6 +31,8 @@ class Test_CoreClass_DBPerf extends UnitTestCase
 		// p2
 		$this->expectException(); DBPerf::fetchCount('test', '');
 		$this->expectException(); DBPerf::fetchCount('test', array());
+		$this->expectException(); DBPerf::fetchCount(1);
+		$this->expectException(); DBPerf::fetchCount(0);
 		$this->expectException(); DBPerf::fetchCount('test', -1);
 	}
 	
@@ -47,16 +49,58 @@ class Test_CoreClass_DBPerf extends UnitTestCase
 	// ---------- counter
 	
 	function testIncrementFetchCount() {
+		$y1 = DBPerf::fetchTotalCount('test');
+		$y2 = DBPerf::fetchTotalCount('test');
+		$this->assertEqual($y1, $y2);
+		
 		$x1 = DBPerf::fetchCount('test');
 		$x2 = DBPerf::fetchCount('test');
 		$this->assertEqual($x1, $x2);
 		$x3 = DBPerf::fetchCount('test', true);
 		$this->assertEqual($x3, DBPerf::fetchCount('test'));
+		
+		$x4 = DBPerf::fetchCachedCount('test');
+		$x5 = DBPerf::fetchCachedCount('test');
+		$this->assertEqual($x4, $x5);
+		$x6 = DBPerf::fetchCachedCount('test', true);
+		$this->assertEqual($x6, DBPerf::fetchCachedCount('test'));
+		
+		$this->assertEqual($y1 + 2, DBPerf::fetchTotalCount('test'));
 	}
 	
-	// TODO
-	// TODO
-	// TODO
+	function testIncrementQueryCount() {
+		$y1 = DBPerf::queryTotalCount('test');
+		$y2 = DBPerf::queryTotalCount('test');
+		$this->assertEqual($y1, $y2);
+		
+		$x1 = DBPerf::queryCount('test');
+		$x2 = DBPerf::queryCount('test');
+		$this->assertEqual($x1, $x2);
+		$x3 = DBPerf::queryCount('test', true);
+		$this->assertEqual($x3, DBPerf::queryCount('test'));
+		
+		$x4 = DBPerf::queryCachedCount('test');
+		$x5 = DBPerf::queryCachedCount('test');
+		$this->assertEqual($x4, $x5);
+		$x6 = DBPerf::queryCachedCount('test', true);
+		$this->assertEqual($x6, DBPerf::queryCachedCount('test'));
+		
+		$this->assertEqual($y1 + 2, DBPerf::queryTotalCount('test'));
+	}
+	
+	function testIncrementTimeCount() {
+		$y1 = DBPerf::execTotalTime('test');
+		$y2 = DBPerf::execTotalTime('test');
+		$this->assertEqual($y1, $y2);
+		
+		$x1 = DBPerf::execTime('test');
+		$x2 = DBPerf::execTime('test');
+		$this->assertEqual($x1, $x2);
+		$x3 = DBPerf::execTime('test', 10);
+		$this->assertEqual($x3, DBPerf::execTime('test'));
+		
+		$this->assertEqual($y1 + 10, DBPerf::execTotalTime('test'));
+	}
 	
 } // Test_CoreClass_DBPerf
 
